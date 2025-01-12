0392ee4ce855cb7a4b2501c387804d01468095bc
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-01-12 21:23:01 +0530

    signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,13 +3,36 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { createUserSchema, SignInUserSchema, createRoomSchema } from "@repo/common/types";
 import { middleware } from "./middleware";
+import { prismaClient } from "@repo/db/client";
 const app = express();
 
-app.post("/signup", (req, res) => {
-    // db call
-    res.json({
-        userId: "123"
-    })
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+
+    const parsedData = createUserSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data?.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            userId: user.id
+        })
+    } catch(e) {
+        res.status(411).json({
+            message: "User already exists with this username"
+        })
+    }
 })
 
 app.post("/signin", (req, res) => {
